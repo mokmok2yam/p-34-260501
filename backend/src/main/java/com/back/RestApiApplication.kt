9d5c3141e42d6354ae4ed22260563689aaffa8f5
HanJeongmok <wjdahe1845@gmail.com>
2026-05-01 12:11:49 +0900

    5,6

--- backend/src/main/java/com/back/RestApiApplication.kt
+++ backend/src/main/java/com/back/RestApiApplication.kt
@@ -1,14 +1,10 @@
 package com.back
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableJpaAuditing
-object RestApiApplication {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        SpringApplication.run(RestApiApplication::class.java, *args)
-    }
+open class RestApiApplication
+fun main (args: Array<String>){
+    runApplication<RestApiApplication>(*args)
 }
